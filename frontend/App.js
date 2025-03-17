7b8d8ac22ea0c54356f03de8b8f0077955d21e40
AnAs101AlAa <anas.ibrahim03@eng-st.cu.edu.eg>
2025-03-17 14:19:36 +0200

    added navigation to frontend

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,33 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const HomeScreen = () => (
+  <View style={styles.container}>
+    <Text>Home Screen</Text>
+    <StatusBar style="auto" />
+  </View>
+);
+
+const DetailsScreen = () => (
+  <View style={styles.container}>
+    <Text>Details Screen</Text>
+    <StatusBar style="auto" />
+  </View>
+);
+
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
