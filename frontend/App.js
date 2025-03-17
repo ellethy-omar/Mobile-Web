e850693353b6d708b53707c15120e11f829583ef
xKimozZ <130156798+xKimozZ@users.noreply.github.com>
2025-03-17 15:30:47 +0200

    Add experimental login/register screen

--- frontend/App.js
+++ frontend/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './LoginScreen';
 
 const HomeScreen = () => (
   <View style={styles.container}>
@@ -23,8 +24,9 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false }} />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
